819d0b683c945190d73f7bd6c2e1a011a56d3dd7
Koske Kimutai <koskedk@live.com>
2018-10-19 23:52:27 +0300

    added index.ts to root

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export * from "./core/model/entity-base";
+export * from "./core/model/aggregate-root";
+export * from "./core/interfaces/irepository-base";
+export * from "./infrastructure/repository-base";
